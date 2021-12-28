a201614a5388ef64d21f7e7fdabee5feb2bf6612
antoniop perez dieppa <aperezdieppa@gmail.com>
2021-12-28 07:40:43 +0000

    community version

--- simpsonsdb-driver/src/main/java/io/mongock/driver/simpsonsdb/driver/SimpsonsDBDriver.java
+++ simpsonsdb-driver/src/main/java/io/mongock/driver/simpsonsdb/driver/SimpsonsDBDriver.java
@@ -3,6 +3,10 @@ package io.mongock.driver.simpsonsdb.driver;
 
 import com.simpsonsdb.SimpsonsDBClient;
 
+import static io.mongock.utils.Constants.DEFAULT_LOCK_ACQUIRED_FOR_MILLIS;
+import static io.mongock.utils.Constants.DEFAULT_QUIT_TRYING_AFTER_MILLIS;
+import static io.mongock.utils.Constants.DEFAULT_TRY_FREQUENCY_MILLIS;
+
 /**
  * This is the class it will be used to instanstiate the driver.
  * You can see an example in the simpsonsdb-springboot-driver, class SpringbootSimpsonsDBContext
--- simpsonsdb-driver/src/main/java/io/mongock/driver/simpsonsdb/driver/SimpsonsDBDriverBase.java
+++ simpsonsdb-driver/src/main/java/io/mongock/driver/simpsonsdb/driver/SimpsonsDBDriverBase.java
@@ -1,69 +1,72 @@
 package io.mongock.driver.simpsonsdb.driver;
 
+import com.simpsonsdb.SimpsonsDBClient;
 import io.mongock.driver.api.entry.ChangeEntryService;
 import io.mongock.driver.core.driver.NonTransactionalConnectionDriverBase;
 import io.mongock.driver.core.lock.LockRepository;
 import io.mongock.driver.simpsonsdb.repository.SimpsonsDBChangeEntryRepository;
 import io.mongock.driver.simpsonsdb.repository.SimpsonsDBLockRepository;
-import com.simpsonsdb.SimpsonsDBClient;
 
 public abstract class SimpsonsDBDriverBase extends NonTransactionalConnectionDriverBase {
 
 
-  private final SimpsonsDBClient client;
-  protected SimpsonsDBChangeEntryRepository changeEntryRepository;
-  protected SimpsonsDBLockRepository lockRepository;
+    private final SimpsonsDBClient client;
+    protected SimpsonsDBChangeEntryRepository changeEntryRepository;
+    protected SimpsonsDBLockRepository lockRepository;
 
-  //Custom fields
-  private String configParameter;
+    //Custom fields
+    private String configParameter;
 
-  /**
-   * This is just an example. It's very likely that something like database engine client will be required in the constructor,
-   * but it may be different or require other parameters.
-   * @param client the database engine client.
-   */
-  protected SimpsonsDBDriverBase(SimpsonsDBClient client,
-                                 long lockAcquiredForMillis,
-                                 long lockQuitTryingAfterMillis,
-                                 long lockTryFrequencyMillis) {
-    super(lockAcquiredForMillis, lockQuitTryingAfterMillis, lockTryFrequencyMillis);
-    this.client = client;
-  }
-
-
-  /**
-   * it returns the lockRepository. If not initialized yet, it's created
-   * @return lockRepository
-   */
-  @Override
-  protected synchronized LockRepository getLockRepository() {
-    if (lockRepository == null) {
-      lockRepository = new SimpsonsDBLockRepository(client.getTable(getLockRepositoryName()));
-      lockRepository.setIndexCreation(isIndexCreation());
+    /**
+     * This is just an example. It's very likely that something like database engine client will be required in the constructor,
+     * but it may be different or require other parameters.
+     *
+     * @param client the database engine client.
+     */
+    protected SimpsonsDBDriverBase(SimpsonsDBClient client,
+                                   long lockAcquiredForMillis,
+                                   long lockQuitTryingAfterMillis,
+                                   long lockTryFrequencyMillis) {
+        super(lockAcquiredForMillis, lockQuitTryingAfterMillis, lockTryFrequencyMillis);
+        this.client = client;
     }
-    return lockRepository;
-  }
 
-  /**
-   * it returns the ChangeEntryRepository. If not initialized yet, it's created
-   * @return ChangeEntryRepository
-   */
-  @Override
-  public synchronized ChangeEntryService getChangeEntryService() {
-    if (changeEntryRepository == null) {
-      changeEntryRepository = new SimpsonsDBChangeEntryRepository(client.getTable(getMigrationRepositoryName()));
-      changeEntryRepository.setIndexCreation(isIndexCreation());
+
+    /**
+     * it returns the lockRepository. If not initialized yet, it's created
+     *
+     * @return lockRepository
+     */
+    @Override
+    protected synchronized LockRepository getLockRepository() {
+        if (lockRepository == null) {
+            lockRepository = new SimpsonsDBLockRepository(client.getTable(getLockRepositoryName()));
+            lockRepository.setIndexCreation(isIndexCreation());
+        }
+        return lockRepository;
     }
-    return changeEntryRepository;
-  }
+
+    /**
+     * it returns the ChangeEntryRepository. If not initialized yet, it's created
+     *
+     * @return ChangeEntryRepository
+     */
+    @Override
+    public synchronized ChangeEntryService getChangeEntryService() {
+        if (changeEntryRepository == null) {
+            changeEntryRepository = new SimpsonsDBChangeEntryRepository(client.getTable(getMigrationRepositoryName()));
+            changeEntryRepository.setIndexCreation(isIndexCreation());
+        }
+        return changeEntryRepository;
+    }
 
 
-  //Custom parameters, specific to this driver
-  public String getConfigParameter() {
-    return configParameter;
-  }
+    //Custom parameters, specific to this driver
+    public String getConfigParameter() {
+        return configParameter;
+    }
 
-  public void setConfigParameter(String configParameter) {
-    this.configParameter = configParameter;
-  }
+    public void setConfigParameter(String configParameter) {
+        this.configParameter = configParameter;
+    }
 }
